a71bdd8c06205c38691041350dd37f73acf1a45d
Josh <dash219@gmail.com>
2019-06-26 15:36:59 -0700

    Random room size and room regeneration working

--- Assets/Scripts/PlaceBlocks.cs
+++ Assets/Scripts/PlaceBlocks.cs
@@ -11,36 +11,41 @@ public class PlaceBlocks : MonoBehaviour
     public Tilemap emptyMap;
     public Tilemap wallMap;
     public GameObject spawnGuy;
+    public Transform playerTf;
+    public int maxEnemies;
     public int totalEnemies;
+    public int totalRooms;
 
     //to-do
-    //  fix noiseOffset, because it depends on square room sizes
-    //  fix corners
-    //  fix doors
+    //  make doors variable size
     List<Vector2Int> RoomGenerator(Vector2Int xyOffset, Vector2Int xyCorner, Vector2Int sizeBoundary, Vector2Int xySizes)
     {
-        int passedSize = xySizes.x;
+        //init basic variables
         xyOffset.x += (xySizes.x * xyCorner.x) + (1 * xyCorner.x);
         xyOffset.y += (xySizes.y * xyCorner.y) + (1 * xyCorner.y);
-        int roomSize = Random.Range(sizeBoundary.x, sizeBoundary.y); //here, we randomly select our room size.
         Vector2Int xyRoomSize = new Vector2Int(Random.Range(sizeBoundary.x, sizeBoundary.y), Random.Range(sizeBoundary.x, sizeBoundary.y));
-        //Vector2Int xyRoomSize = new Vector2Int(roomSize, roomSize);
         if ((xyRoomSize.x < 0) || (xyRoomSize.y < 0))
             return new List<Vector2Int> { new Vector2Int(0, 0), new Vector2Int(0, 0) };
+
+        //set up noise
         int noiseDirection = Random.Range(-10, 10); //the noiseX variables add variation to room placement
         if (noiseDirection < 0)
             noiseDirection = -1;
         if (noiseDirection > 0)
             noiseDirection = 1;
-        int noiseOffset = Random.Range(0, passedSize - roomSize) * noiseDirection; //currently broken
-        noiseOffset = 0; //debug line
+        int noiseOffset = 0;
+        if(xyCorner.x != 0) // if horiz room
+            {noiseOffset = Random.Range(1, xySizes.y - xyRoomSize.y) * noiseDirection;}
+        else if (xyCorner.y != 0) // if vert room
+            {noiseOffset = Random.Range(1, xySizes.x - xyRoomSize.x) * noiseDirection;}
         xyOffset.x += (xyRoomSize.x * xyCorner.x) + (noiseOffset * xyCorner.y);
         xyOffset.y += (xyRoomSize.y * xyCorner.y) + (noiseOffset * xyCorner.x);
 
-        if (totalEnemies > 0)
+        //spawn enemies
+        if (totalEnemies < maxEnemies)
         {
             Instantiate(spawnGuy, new Vector3(xyOffset.x, xyOffset.y, 0), Quaternion.identity);
-            totalEnemies--;
+            totalEnemies++;
         }
 
         //make wall outline
@@ -62,15 +67,15 @@ public class PlaceBlocks : MonoBehaviour
                 if ((wallMap.GetTile(pos) == null) && (emptyMap.GetTile(pos) == null))
                 { wallMap.SetTile(pos, wallTile); }
             }
-            /*
+            
             //corners
             for (int i = -1; i < 2; i += 2)
             {
-                Vector3Int cornerVec = new Vector3Int(xyOffset.x + (roomSize * j) - (j == -1 ? 1 : 0),
-                                                    xyOffset.y + (roomSize * i) - (i == -1 ? 1 : 0), 0);
+                Vector3Int cornerVec = new Vector3Int(xyOffset.x + (xyRoomSize.x * j) - (j == -1 ? 1 : 0),
+                                                    xyOffset.y + (xyRoomSize.y * i) - (i == -1 ? 1 : 0), 0);
                 if((wallMap.GetTile(cornerVec) == null) && (emptyMap.GetTile(cornerVec) == null))
                     wallMap.SetTile(cornerVec, wallTile2);
-            }*/
+            }
         }
         for(int i = -xyRoomSize.x; i < xyRoomSize.x; i++)
         {
@@ -86,11 +91,11 @@ public class PlaceBlocks : MonoBehaviour
         int doorLoc;
         if (xyCorner.x != 0) //if we have a horizontally placed room
         {
-            int xOffset = xyOffset.x - (xyCorner.x * roomSize) - ((xyCorner.x == 1) ? 1 : 0);
+            int xOffset = xyOffset.x - (xyCorner.x * xyRoomSize.x) - ((xyCorner.x == 1) ? 1 : 0);
             int yLower = 0;
             int yUpper = 0;
             Vector3Int savedVec = new Vector3Int(xOffset - xyCorner.x, 0, 0);
-            for (int i = xyOffset.y - (roomSize - 1); i < xyOffset.y + (roomSize); i++)
+            for (int i = xyOffset.y - (xyRoomSize.y - 1); i < xyOffset.y + (xyRoomSize.y); i++)
             {
                 savedVec.y = i;
                 if (yLower == 0)
@@ -105,7 +110,7 @@ public class PlaceBlocks : MonoBehaviour
                     if (emptyMap.GetTile(savedVec) != null)
                         yUpper = i;
                     else
-                        i = xyOffset.y + roomSize;
+                        i = xyOffset.y + xyRoomSize.y;
                 }
             }
             doorLoc = Random.Range(yLower, yUpper);
@@ -115,11 +120,11 @@ public class PlaceBlocks : MonoBehaviour
         }
         else if (xyCorner.y != 0) //if we have a vertically placed room
         {
-            int yOffset = xyOffset.y - (xyCorner.y * roomSize) - ((xyCorner.y == 1) ? 1 : 0);
+            int yOffset = xyOffset.y - (xyCorner.y * xyRoomSize.y) - ((xyCorner.y == 1) ? 1 : 0);
             int xLower = 0;
             int xUpper = 0;
             Vector3Int savedVec = new Vector3Int(0, yOffset - xyCorner.y, 0);
-            for (int i = xyOffset.x - (roomSize - 1); i < xyOffset.x + (roomSize); i++)
+            for (int i = xyOffset.x - (xyRoomSize.x - 1); i < xyOffset.x + (xyRoomSize.x); i++)
             {
                 savedVec.x = i;
                 if (xLower == 0)
@@ -134,7 +139,7 @@ public class PlaceBlocks : MonoBehaviour
                     if (emptyMap.GetTile(savedVec) != null)
                         xUpper = i;
                     else
-                        i = xyOffset.x + roomSize;
+                        i = xyOffset.x + xyRoomSize.x;
                 }
             }
             doorLoc = Random.Range(xLower, xUpper);
@@ -147,10 +152,11 @@ public class PlaceBlocks : MonoBehaviour
     {
         Vector2Int lastDir = new Vector2Int(0, 0);
         Vector2Int randomDir = new Vector2Int(0, 0);
+        int maxAttempts = 3;
         while (totalRooms > 0)
         {
-            int attempts = 0;
-            while ((lastDir == randomDir) && (attempts < 3))
+            int currAttempts = 0;
+            while ((lastDir == randomDir) && (currAttempts < maxAttempts))
             {
                 randomDir = new Vector2Int(0, 0);
                 randomDir.x = Random.Range(-1, 1);
@@ -160,7 +166,7 @@ public class PlaceBlocks : MonoBehaviour
                     if (randomDir.y == 0)
                         randomDir.y = 1;
                 }
-                attempts++;
+                currAttempts++;
             }
             List<Vector2Int> funcOutput = RoomGenerator(xyOffset, randomDir, sizeBoundary, xySize);
             xyOffset = funcOutput[0];
@@ -171,30 +177,38 @@ public class PlaceBlocks : MonoBehaviour
         }
     }
 
+    void worldGeneratorHelper(int xOffset, int yOffset)
+    {
+        //generate starting room
+        List<Vector2Int> savedData = RoomGenerator(new Vector2Int(xOffset, yOffset), new Vector2Int(0, 0), new Vector2Int(5, 5), new Vector2Int(0, 0));
+        //start world generation
+        worldGenerator(savedData[0], new Vector2Int(2, 12), savedData[1], totalRooms);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        totalEnemies = 10;
-        //init starting platform
-        for (int i = -2; i < 2; i++)
+        totalEnemies = 0;
+        maxEnemies = 10;
+        totalRooms = 50;
+        worldGeneratorHelper(0, 0);
+    }
+    
+    void Update()
+    {
+        bool keyReset = Input.GetButtonDown("Fire2");
+        if (keyReset)
         {
-            Vector3Int pos = new Vector3Int(i, -2, 0);
-            wallMap.SetTile(pos, wallTile);
-        }
-        //generate map border
-        List<Vector2Int> savedData = RoomGenerator(new Vector2Int(0, 0), new Vector2Int(0, 0), new Vector2Int(5, 5), new Vector2Int(0,0));
+            Vector3Int playerTileCoord = emptyMap.WorldToCell(playerTf.position);
+            emptyMap.ClearAllTiles();
+            wallMap.ClearAllTiles();
 
-        worldGenerator(savedData[0], new Vector2Int(2, 12), savedData[1], 50);
-        /*
-        Vector3Int tempSave = RoomGenerator(new Vector3Int(0,0, savedData.z), new Vector2Int(0, 1), 3, 5);
-        RoomGenerator(tempSave, new Vector2Int(-1, 0), 3, 5);
-        tempSave = RoomGenerator(new Vector3Int(0,0, savedData.z), new Vector2Int(-1, 0), 3, 5);
-        //ughhhhhhhhhhhhh
-        //Debug.Log(RoomGenerator(tempSave, new Vector2Int(0, 1), 3, 5));
-        for (int i = 0; i < 25; i++)
-            {savedData = RoomGenerator(savedData, new Vector2Int(1, 0), 2, Mathf.Min(savedData.z + 4, 12));}
-        for (int i = 0; i < 25; i++)
-            {savedData = RoomGenerator(savedData, new Vector2Int(0,-1), 2, Mathf.Min(savedData.z + 4, 12));}
-        */
+            //var EnemyObj = GameObject.FindGameObjectsWithTag("BasicGuy");
+            //foreach (GameObject item in EnemyObj)
+            //    {Destroy(item);}
+            //totalEnemies = 0;
+
+            worldGeneratorHelper(playerTileCoord.x, playerTileCoord.y);
+        }
     }
 }
